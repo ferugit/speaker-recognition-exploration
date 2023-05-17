7d8d8e2690606243705625b06e951ff408312c78
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-05-17 14:58:54 +0200

    add streaming metrics

---
+++ src/utils/streaming_metrics.py
@@ -0,0 +1,34 @@
+
+# Fernando López Gavilánez, 2023
+
+# False Alarm (FA) = False Positives (FP)
+# False Rejection (FR) = False Negatives (FN) = Misses 
+
+
+def measure_frame_errors(gt, predictions):
+    assert len(gt) == len(predictions), "length of ground truth list and predictions must be the same"
+    fp, fn = 0, 0
+    for i in range(len(gt)):
+        if gt[i] != predictions[i]:
+            if gt[i] == 1:
+                fn += 1
+            else:
+                fp += 1
+    return fp, fn
+
+
+def get_frame_error_rate(gt, predictions):
+    fp, fn = measure_frame_errors(gt, predictions)
+    return 100*(fp+fn)/len(gt)
+
+
+def get_detect_cost_function(gt, predictions, λ=0.5):
+    fp, fn = measure_frame_errors(gt, predictions)
+    return (1-λ)*fn + λ*fp
+
+
+def get_fer_and_dct(gt, predictions, λ=0.5):
+    fp, fn = measure_frame_errors(gt, predictions)
+    fer = 100*(fp+fn)/len(gt)
+    dct = (1-λ)*fn + λ*fp
+    return fer, dct
