309883525fe81220ec1e8f0c8f7b9c1d622492df
omerhatim <38071904+omerhatim@users.noreply.github.com>
2023-07-11 13:21:07 +0200

    Delete eer.sh

--- eer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python -u eer.py --prefix "clips/" --savedfile "clips.tsv"
-python -u eer.py --prefix "clips1/" --savedfile "clips1.tsv"
-python -u eer.py --prefix "clips2/" --savedfile "clips2.tsv"
-python -u eer.py --prefix "clips3/" --savedfile "clips3.tsv"
